37f94440195717418d21eb9c696760036b777ef3
Tommy Trevino <tommy@vantumstrategies.com>
2026-08-14 17:40:08 -0500

    build: hardcode PORT to 9377 to align with Docker EXPOSE port

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { Camoufox } from 'camoufox-js';
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT || 9377;
+const PORT = 9377;
 const ACCESS_KEY = process.env.PHOTO_RESOLVER_TOKEN || process.env.CAMOFOX_ACCESS_KEY;
 
 // Browser launch options
